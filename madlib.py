9bb42a2395dd15c2270d6638c893b45fe0a1126b
ciattaa <53193776+ciattaa@users.noreply.github.com>
2019-07-22 15:54:13 -0600

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("Please enter a name" )
+    place =  input ("Please enter a place") 
+    vehicle = input("Please enter a name of a vehicle")
+    verb = input("Please enter a verb")
+    adverb = input("Please enter a adverb")
+    print (name + " went to visit their friends at " + place +  " While he was there, he saw a " + vehicle + verb + adverb + " While unsure of what was happening at first, " + name + " soon found out that is was a street race! ")
+if __name__ == "__main__":
+    main()
